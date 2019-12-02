038b0ebba47309e544ea1780ff7fbc99a2a0aec3
s08glen <s08glen@gmail.com>
2019-12-03 01:19:17 +0800

    my fsm test1

--- utils.py
+++ utils.py
@@ -2,8 +2,7 @@ import os
 
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from linebot.models import MessageEvent, PostbackEvent, TextSendMessage, TemplateSendMessage,
-                            TextMessage, ButtonsTemplate,PostbackTemplateAction, MessageTemplateAction, URITemplateAction,ImageSendMessage
+from linebot.models import MessageEvent, PostbackEvent, TextSendMessage, TemplateSendMessage, TextMessage, ButtonsTemplate,PostbackTemplateAction, MessageTemplateAction, URITemplateAction,ImageSendMessage
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
