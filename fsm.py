900dbb8922626f30d76ccba0d9324af4d6deaa5e
s08glen <s08glen@gmail.com>
2019-12-03 17:15:24 +0800

    id template test

--- fsm.py
+++ fsm.py
@@ -1,7 +1,7 @@
 from transitions.extensions import GraphMachine
 
 
-from utils import send_text_message, send_image_url,send_template
+from utils import send_text_message, send_image_url,reply_template,push_template
 from linebot.models import MessageEvent, PostbackEvent, TextSendMessage, TemplateSendMessage, ButtonsTemplate,PostbackTemplateAction, MessageTemplateAction, URITemplateAction,ImageSendMessage
 
 
