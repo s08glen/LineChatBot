ca80b2d6191b23e82fa63179e4eee1c83492fdf2
s08glen <s08glen@gmail.com>
2019-12-04 22:42:41 +0800

    template test2

--- fsm.py
+++ fsm.py
@@ -10,13 +10,13 @@ from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 
 def movie():
-    target_url = 'https://movies.yahoo.com.tw/'
+    target_url = 'https://www.ptt.cc/bbs/CVS/index.html'
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'html.parser')   
     content = ""
-    for index, data in enumerate(soup.select('div.movielist_info h1 a')):
+    for index, data in enumerate(soup.select('div.title a')):
         if index == 20:
             return content       
         title = data.text
